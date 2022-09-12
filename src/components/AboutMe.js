abc0d78f71d0cc0052f14a16cfae05e1cbde104e
Eliott Gcv <eliott.gcv@gmail.com>
2022-09-12 18:11:43 +0200

    Add photo on about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import {Col, Container, Row} from "react-bootstrap";
+import imgMe from "../assets/img/me.jpeg";
 
 export const AboutMe = () => {
     return (
@@ -7,6 +8,7 @@ export const AboutMe = () => {
                 <Row>
                     <Col>
                         <h2>About Me</h2>
+                        <img className="me-img img-fluid rounded-circle" src={imgMe}  alt="image url"/>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     </Col>
                 </Row>
